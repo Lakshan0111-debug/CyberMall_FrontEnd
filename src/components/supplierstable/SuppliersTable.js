de39230b5587e96f33468bc1df26875e74b7a280
chinthaka Lakshan <diluklakshan01@gmail.com>
2024-10-21 00:20:01 +0530

    supplier table backend connect

--- src/components/supplierstable/SuppliersTable.js
+++ src/components/supplierstable/SuppliersTable.js
@@ -1,30 +1,53 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { DataGrid } from "@mui/x-data-grid";
-import { supplierRows } from "../../SuppliersTableSource";
 import "./SuppliersTable.css";
 import { Link } from "react-router-dom";
+import axios from "axios";
 
 const SuppliersTable = () => {
-  const [data] = useState(supplierRows);
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const fetchSuppliers = async () => {
+      try {
+        const response = await axios.get("http://localhost:8080/suppliers");
+        const suppliersWithId = response.data.map((supplier) => ({
+          ...supplier,
+          id: supplier.supplierId, // Ensure id is mapped correctly
+        }));
+        setData(suppliersWithId);
+      } catch (error) {
+        console.error("Error fetching supplier data", error);
+      }
+    };
+    fetchSuppliers();
+  }, []);
+
+  const handleDelete = (id) => {
+    if (window.confirm("Are you sure you want to delete this supplier?")) {
+      axios
+        .delete(`http://localhost:8080/suppliers/${id}`)
+        .then(() => {
+          // Remove supplier from the data list
+          setData(data.filter((item) => item.id !== id)); // Correct the field
+          alert("Supplier deleted successfully");
+        })
+        .catch((error) => {
+          console.error("There was an error deleting the supplier!", error);
+        });
+    }
+  };
 
   const supplierColumns = [
     { field: "id", headerName: "Supplier ID", width: 100 },
     {
-      field: "supplier",
+      field: "supplierName",
       headerName: "Supplier",
       width: 230,
-      renderCell: (params) => {
-        return (
-          <div className="cellWithImg">
-            <img className="cellImg" src={params.row.img} alt="avatar" />
-            {params.row.supplier}
-          </div>
-        );
-      },
     },
     { field: "email", headerName: "E-mail", width: 150 },
     { field: "phoneNo", headerName: "Phone No", width: 120 },
-    { field: "address", headerName: "Address", width: 200, },
+    { field: "address", headerName: "Address", width: 200 },
   ];
 
   const actionColumn = [
@@ -35,10 +58,12 @@ const SuppliersTable = () => {
       renderCell: (params) => {
         return (
           <div className="cellAction">
-            <Link to="/manageSuppliers/supplierId" style={{ textDecoration: "none" }}>
+            <Link to={`/manageSuppliers/${params.row.id}`} style={{ textDecoration: "none" }}>
               <div className="viewButton">VIEW</div>
             </Link>
-            <div className="deleteButton">DELETE</div>
+            <div className="deleteButton" onClick={() => handleDelete(params.row.id)}>
+              DELETE
+            </div>
           </div>
         );
       },
@@ -59,6 +84,7 @@ const SuppliersTable = () => {
         columns={supplierColumns.concat(actionColumn)}
         pageSize={7}
         rowsPerPageOptions={[5]}
+        getRowId={(row) => row.id} 
       />
     </div>
   );
