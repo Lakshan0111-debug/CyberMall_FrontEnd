48dd65e39246a393b912de5a6787853f25b90d04
Sachintha Rashen Bandara <srashenb@gmail.com>
2024-10-22 01:57:23 +0530

    Place Order Page

--- src/pages/Cart/Cart.js
+++ src/pages/Cart/Cart.js
@@ -7,13 +7,13 @@ import { useNavigate } from 'react-router-dom';
 
 const Cart = () => {
 
-  const {cartItems, products_list, removeFromCart, getTotalCartAmount} = useContext(StoreContext);
+  const { cartItems, products_list, removeFromCart, getTotalCartAmount } = useContext(StoreContext);
 
   const navigate = useNavigate();
 
   return (
     <div className='cart'>
-      <Navbar/>
+      <Navbar />
       <div className='cart-items'>
         <div className='cart-items-title'>
           <p>Items</p>
@@ -23,23 +23,26 @@ const Cart = () => {
           <p>Total</p>
           <p>Remove</p>
         </div>
-        <br/>
-        <hr/>
-        {products_list.map((item,index)=>{
-          if(cartItems[item._id]>0){
+        <br />
+        <hr />
+        {products_list.map((item, index) => {
+          if (cartItems[item._id] > 0) {
             return (
               <div>
                 <div className='cart-items-title cart-items-item'>
-                  <img src={item.image} alt=''/>
+                  <img src={item.image} alt='' />
                   <p>{item.name}</p>
                   <p>LKR.{item.price}</p>
                   <p>{cartItems[item._id]}</p>
-                  <p>LKR.{item.price*cartItems[item._id]}</p>
-                  <p onClick={()=>removeFromCart(item._id)} className='cross'>x</p>
+                  <p>LKR.{item.price * cartItems[item._id]}</p>
+                  <p onClick={() => removeFromCart(item._id)} className='cross'>x</p>
                 </div>
-                <hr/>
+                <hr />
               </div>
             )
+          }
+          else {
+            return null
           }
         })}
       </div>
@@ -51,21 +54,21 @@ const Cart = () => {
               <p>Subtotal</p>
               <p>LKR.{getTotalCartAmount()}</p>
             </div>
-            <hr/>
+            <hr />
             <div className='cart-total-details'>
               <p>Delivery Fee</p>
-              <p>LKR.{2}</p>
+              <p>LKR.{getTotalCartAmount() === 0 ? 0 : 2}</p>
             </div>
-            <hr/>
+            <hr />
             <div className='cart-total-details'>
               <b>Total</b>
-              <b>LKR.{getTotalCartAmount()+2}</b>
+              <b>LKR.{getTotalCartAmount() === 0 ? 0 : getTotalCartAmount() + 2}</b>
             </div>
           </div>
-          <button onClick={()=>navigate('/placeOrder')}>PROCEED TO CHECKOUT</button>
+          <button onClick={() => navigate('/placeOrder')}>PROCEED TO CHECKOUT</button>
         </div>
       </div>
-      <Footer/>
+      <Footer />
     </div>
   )
 }
--- src/pages/PlaceOrder/PlaceOrder.js
+++ src/pages/PlaceOrder/PlaceOrder.js
@@ -1,29 +1,52 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import './PlaceOrder.css';
 import Navbar from '../../components/Navbar/Navbar';
 import Footer from '../../components/Footer/Footer';
+import { StoreContext } from '../../context/StoreContext';
 
 const PlaceOrder = () => {
+
+  const {getTotalCartAmount} = useContext(StoreContext)
+
   return (
-    <>
-    <Navbar/>
-    <form className='place-order'>
-      <div className='place-order-left'>
-        <p className='title'>Delivery Information</p>
-        <div className='multi-fields'>
-          <input type='text' placeholder='First Name'/>
-          <input type='text' placeholder='Last Name'/>
+    <div className='place-order-page'>
+      <Navbar/>
+      <form className='place-order'>
+        <div className='place-order-left'>
+          <p className='title'>Delivery Information</p>
+          <div className='multi-fields'>
+            <input type='text' placeholder='First Name'/>
+            <input type='text' placeholder='Last Name'/>
+          </div>
+          <input type='email' placeholder='E-Mail Address'/>
+          <input type='text' placeholder='Shipping Address'/>
+          <input type='text' placeholder='Phone'/>
         </div>
-        <input type='email' placeholder='E-Mail Address'/>
-        <input type='text' placeholder='Shipping Address'/>
-        <input type='text' placeholder='Phone'/>
-      </div>
-      <div className='place-order-right'>
-        
-      </div>
-    </form>
-    <Footer/>
-    </>
+        <div className='place-order-right'>
+          <div className='cart-total'>
+            <h2>Cart Totals</h2>
+            <div>
+              <div className='cart-total-details'>
+                <p>Subtotal</p>
+                <p>LKR.{getTotalCartAmount()}</p>
+              </div>
+              <hr/>
+              <div className='cart-total-details'>
+                <p>Delivery Fee</p>
+                <p>LKR.{getTotalCartAmount()===0?0:2}</p>
+              </div>
+              <hr/>
+              <div className='cart-total-details'>
+                <b>Total</b>
+                <b>LKR.{getTotalCartAmount()===0?0:getTotalCartAmount()+2}</b>
+              </div>
+            </div>
+            <button>PROCEED TO PAYMENT</button>
+          </div>
+        </div>
+      </form>
+      <Footer/>
+    </div>
   )
 }
 
